e932cc1127a3b34bd681bb3f48443cb24930c3bf
MarianaGivisiez <80357554+MarianaGivisiez@users.noreply.github.com>
2024-04-13 16:11:56 -0300

    Conecta com Database no main

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,19 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from interface_registro import Ui_MainWindow
+import interface_registro
+from database import DataBase
+
+db = DataBase()
 
 def main():
     app_principal = QApplication(sys.argv)
     janela = QMainWindow()
-    tela_registro = Ui_MainWindow()
+    db.connect()
+    db.create_tables()
+    tela_registro = interface_registro.TelaRegistro(db)
     tela_registro.setupUi(janela)
     janela.show()
+    #db.close_connection()
     sys.exit(app_principal.exec_())
 
 if __name__ == '__main__':
