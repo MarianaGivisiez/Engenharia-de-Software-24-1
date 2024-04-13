e1b7bee3a38a3a3b1621e93adc4ae1a34397f446
Jvsaade <jvssimao@gmail.com>
2024-04-13 13:58:04 -0300

    Atualizando main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,11 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from interface_registro import TelaRegistro
+from interface_registro import Ui_MainWindow
 
 def main():
     app_principal = QApplication(sys.argv)
     janela = QMainWindow()
-    tela_registro = TelaRegistro()
+    tela_registro = Ui_MainWindow()
     tela_registro.setupUi(janela)
     janela.show()
     sys.exit(app_principal.exec_())
