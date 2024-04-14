31c6733877e0c78a142bfde384b7537c8a4c9e12
Thiago Santos <47781320+thiagoolvr@users.noreply.github.com>
2024-04-13 21:58:53 -0300

    adiciona funcionalidade de exibir/ocultar senha na tela de login

---
+++ scripts/interface_login.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5 import uic
+from PyQt5 import QtGui
+import os
+
+class TelaLogin(QMainWindow):
+
+    def __init__(self):
+        super(TelaLogin, self).__init__()
+        base_path = os.path.dirname(__file__)
+        file_path = os.path.join(base_path, "../interfaces/tela_login.ui")
+        uic.loadUi(file_path, self)
+
+        img_path = os.path.join(base_path, "eye.png")
+        self.iconShow = QtGui.QIcon(img_path)
+        img_path = os.path.join(base_path, "eye_blind.png")
+        self.iconHide = QtGui.QIcon(img_path)
+
+        self.lineEdit_2.setEchoMode(QLineEdit.Password)
+        self.pushButton_3.setIcon(self.iconShow)
+        self.pushButton_3.clicked.connect(self.toggleVisibility)
+       
+        self.show()
+    
+    def toggleVisibility(self):
+            if self.lineEdit_2.echoMode()==QLineEdit.Normal:
+                self.lineEdit_2.setEchoMode(QLineEdit.Password)
+                self.pushButton_3.setIcon(self.iconShow)
+            else:
+                self.lineEdit_2.setEchoMode(QLineEdit.Normal)
+                self.pushButton_3.setIcon(self.iconHide)
